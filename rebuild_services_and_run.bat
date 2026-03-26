e0179a8eeadf1ceda82be52dfb732ac69711b83c
hila aharonov <bis-hilaa@greendreamteam.onmicrosoft.com>
2026-03-26 16:52:14 +0200

    add run script

---
+++ rebuild_services_and_run.bat
@@ -0,0 +1,8 @@
+ cd C:\Users\hilaa\datamesh
+ echo building team team_service
+ docker build -f .\team\Dockerfile -t datamesh-team_service:latest . --no-cache
+
+ echo building mesh service
+ docker build -f .\mesh\Dockerfile -t datamesh-mesh:latest . --no-cache
+
+ docker compose up
