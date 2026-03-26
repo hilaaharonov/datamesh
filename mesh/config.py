c1524ee21ece8b6a53e17f464d3378b47e0e12f9
hila aharonov <bis-hilaa@greendreamteam.onmicrosoft.com>
2026-03-26 16:24:19 +0200

    docker fix TEAM URL

--- mesh/config.py
+++ mesh/config.py
@@ -5,11 +5,12 @@ ARANGO_URL      = os.getenv("ARANGO_URL", "http://localhost:8529")
 ARANGO_USER     = os.getenv("ARANGO_USER", "root")
 ARANGO_PASSWORD = os.getenv("ARANGO_PASSWORD", "rootpassword")
 ARANGO_DB       = os.getenv("ARANGO_DB", "data_mesh")
+TEAM_SERVICE_URL       = os.getenv("TEAM_SERVICE_URL", "http://team_service:8001/data")
 
 DATA_PRODUCTS: list[DataProduct] = [
     DataProduct(
         name="team",
-        get_products_url="http://localhost:8001/data",
+        get_products_url=TEAM_SERVICE_URL,
         interval_seconds=60,
         collection="product_team",
     ),
